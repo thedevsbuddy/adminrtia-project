88c2a058ac1592a353fadcf6f66221f085c24f77
iamspydey <shoabkhan33@gmail.com>
2023-01-14 12:10:44 +0530

    Update Header.vue

--- resources/js/Components/Adminr/Header.vue
+++ resources/js/Components/Adminr/Header.vue
@@ -3,19 +3,19 @@
     <nav class="px-4 py-2 flex items-center justify-between h-full">
       <ul class="flex h-full items-center space-x-2 ">
         <li>
-          <Link class="block" href="#">
+          <button class="block" @click="$emit('toggleSidebar')">
             <icon name="menu" class="w-6 h-6"/>
-          </Link>
+          </button>
         </li>
       </ul>
       <div class="flex items-center space-x-3">
         <div
-            class="group bg-slate-900 hover:bg-slate-700 transition ease-in-out transition-colors duration-300  cursor-pointer rounded-lg w-56 h-8 flex items-center justify-between px-2">
+            class="group bg-slate-900 hover:bg-slate-700 transition ease-in-out transition-colors duration-300  cursor-pointer rounded-lg w-56 h-8 flex items-center justify-between px-2 py-1">
           <div class="flex items-center">
             <icon name="search"/>
             <span class="text-sm pl-1">Search here...</span>
           </div>
-          <span class="rounded bg-slate-700 transition ease-in-out transition-colors duration-300 group-hover:bg-slate-900  rounded-lg px-2 -mr-1">Ctrl+k</span>
+          <span class="rounded bg-slate-700 transition ease-in-out transition-colors duration-300 group-hover:bg-slate-900  rounded-lg px-2 -mr-1 text-xs tracking-wider uppercase h-full flex items-center">Ctrl+k</span>
         </div>
         <Link href="#" class="flex h-8 items-center bg-slate-900 text-light rounded-lg py-1 px-4 text-sm font-medium space-x-1">
           <icon name="builder" class="w-4 h-4"/>
@@ -37,7 +37,9 @@
   </div>
 </template>
 <script setup>
-import {defineProps, onMounted, ref} from "vue";
+import {onMounted, ref} from "vue";
+
+defineEmits(['toggleSidebar']);
 
 const isSearchOpened = ref(false);
 const props = defineProps({
