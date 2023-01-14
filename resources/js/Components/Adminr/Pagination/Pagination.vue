d234f9d13f7a87aa99b981bed71257ab824cceb4
iamspydey <shoabkhan33@gmail.com>
2023-01-14 12:10:33 +0530

    Added pagination component

---
+++ resources/js/Components/Adminr/Pagination/Pagination.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="inline-flex items-center rounded-lg border divide-x">
+    <pagination-item v-for="uri in props.links" :href="uri.url" :disabled="uri.url == null"  :active="uri.active" :key="uri.label" v-html="uri.label" />
+  </div>
+</template>
+<script setup>
+const props = defineProps({
+  links: Array,
+})
+</script>
---
+++ resources/js/Components/Adminr/Pagination/PaginationItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <Link :class="`${props.active ? 'bg-primary border-primary text-white cursor-default' : props.disabled ? 'bg-slate-200 text-slate-600 pointer-events-none' : 'bg-white hover:bg-light text-slate-600'} text-sm font-semibold flex items-center justify-center px-4 py-2 first:rounded-l-lg last:rounded-r-lg ${props.disabled ? 'cursor-not-allowed' : ''}`" :disabled="props.disabled">
+    <slot />
+  </Link>
+</template>
+<script setup>
+const props = defineProps({
+  active: {
+    type: Boolean,
+    default: false,
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+})
+</script>
